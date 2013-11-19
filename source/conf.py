fb75faf3a32d4e80f9db1d6dfa85ca08b09a47e7
Jiang Xin <worldhello.net@gmail.com>
2013-11-20 07:17:44 +0800

    Add theme for raspi from my gotgithub template
    Signed-off-by: Jiang Xin <worldhello.net@gmail.com>

--- source/conf.py
+++ source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'raspi'
-copyright = u'2013, Jiang Xin'
+copyright = u'2013, 蒋鑫'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -98,7 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'raspi'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -106,7 +106,7 @@ html_theme = 'default'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ["../theme"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
